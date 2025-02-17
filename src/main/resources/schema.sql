9ea6910871d09b35fc958f901b1add1ce7aa29c6
JunhaumHayden <junhaumhayden@hotmail.com>
2025-02-17 00:12:09 -0300

    teste de branch

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,5 @@
-CREATE TABLE usuario(
+-- Tabela Cliente
+CREATE TABLE IF NOT EXISTS cliente (
     id INT NOT NULL AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
@@ -7,28 +8,23 @@ CREATE TABLE usuario(
     cep VARCHAR(9),
     complemento_endereco VARCHAR(100),
     numero_endereco VARCHAR(10),
-    tipo VARCHAR(12) NOT NULL,
-    CONSTRAINT pk_usuario PRIMARY KEY(id)
-);
-
-CREATE TABLE cliente(
-    id_usuario INT NOT NULL REFERENCES usuario(id),
     cpf VARCHAR(11) NOT NULL UNIQUE,
-    nascimento TIMESTAMP,
-    CONSTRAINT pk_cliente PRIMARY KEY (id_usuario),
-    CONSTRAINT fk_cliente_usuario FOREIGN KEY (id_usuario) REFERENCES usuario(id)
-     ON DELETE CASCADE
-     ON UPDATE CASCADE
+    CONSTRAINT pk_cliente PRIMARY KEY(id)
 );
 
-CREATE TABLE profissional(
-    id_usuario INT NOT NULL REFERENCES usuario(id),
+-- Tabela Profissional
+CREATE TABLE IF NOT EXISTS profissional (
+    id INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(256) NOT NULL,
+    telefone VARCHAR(15),
+    cep VARCHAR(9),
+    numero_endereco VARCHAR(10),
+    complemento_endereco VARCHAR(100),
     especialidade VARCHAR(100) NOT NULL,
     registro_profissional VARCHAR(50) UNIQUE,
-    CONSTRAINT pk_profissional PRIMARY KEY (id_usuario),
-    CONSTRAINT fk_profissional_usuario FOREIGN KEY (id_usuario) REFERENCES usuario(id)
-      ON DELETE CASCADE
-      ON UPDATE CASCADE
+    CONSTRAINT pk_profissional PRIMARY KEY (id)
 );
 
 -- Tabela Servico
@@ -42,7 +38,7 @@ CREATE TABLE IF NOT EXISTS servico (
     situacao ENUM('ATIVO', 'INATIVO', 'BLOQUEADO') NOT NULL DEFAULT 'BLOQUEADO',
     profissional_id INT NOT NULL,
     CONSTRAINT pk_servico PRIMARY KEY (id),
-    CONSTRAINT fk_servico_profissional FOREIGN KEY (profissional_id) REFERENCES profissional(id_usuario)
+    CONSTRAINT fk_servico_profissional FOREIGN KEY (profissional_id) REFERENCES profissional(id)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
@@ -53,9 +49,8 @@ CREATE TABLE IF NOT EXISTS agendamento (
     cliente_id INT NOT NULL,
     servico_id INT NOT NULL,
     data_hora TIMESTAMP NOT NULL,
-    status ENUM('CONFIRMADO', 'PENDENTE', 'CANCELADO', 'ATENDIDO') NOT NULL DEFAULT 'PENDENTE',
     CONSTRAINT pk_agendamento PRIMARY KEY (id),
-    CONSTRAINT fk_agendamento_cliente FOREIGN KEY (cliente_id) REFERENCES cliente(id_usuario)
+    CONSTRAINT fk_agendamento_cliente FOREIGN KEY (cliente_id) REFERENCES cliente(id)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
     CONSTRAINT fk_agendamento_servico FOREIGN KEY (servico_id) REFERENCES servico(id)
@@ -63,72 +58,33 @@ CREATE TABLE IF NOT EXISTS agendamento (
     ON UPDATE CASCADE
     );
 
--- Tabela Avaliacao
-CREATE TABLE IF NOT EXISTS avaliacao (
-    id INT AUTO_INCREMENT,
-    cliente_id INT NOT NULL,
-    servico_id INT NOT NULL,
-    data_hora TIMESTAMP NOT NULL,
-    comentario TEXT,
-    nota int,
-    CONSTRAINT pk_avaliacao PRIMARY KEY (id),
-    CONSTRAINT fk_avaliacao_cliente FOREIGN KEY (cliente_id) REFERENCES cliente(id_usuario)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-    CONSTRAINT fk_avaliacao_servico FOREIGN KEY (servico_id) REFERENCES servico(id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE
-    );
-
--- Tabela Foto
-CREATE TABLE IF NOT EXISTS foto_usuario (
-    id INT AUTO_INCREMENT,
-    usuario_id INT NOT NULL,
-    foto BLOB NOT NULL,
-    CONSTRAINT pk_foto_usuario PRIMARY KEY (id),
-    CONSTRAINT fk_foto_usuario_usuario FOREIGN KEY (usuario_id) REFERENCES usuario(id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE
-    );
-
--- Tabela Foto
-CREATE TABLE IF NOT EXISTS foto_servico (
-    id INT AUTO_INCREMENT,
-    servico_id INT NOT NULL,
-    foto BLOB NOT NULL,
-    CONSTRAINT pk_foto_servico PRIMARY KEY (id),
-    CONSTRAINT fk_foto_servico_servico FOREIGN KEY (servico_id) REFERENCES servico(id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE
-    );
-
--- -- Tabela FotoCliente
--- CREATE TABLE IF NOT EXISTS foto_cliente (
---     id INT AUTO_INCREMENT,
---     cliente_id INT NOT NULL,
---     foto BLOB NOT NULL,
---     CONSTRAINT pk_foto_cliente PRIMARY KEY (id),
---     CONSTRAINT fk_foto_cliente_cliente FOREIGN KEY (cliente_id) REFERENCES cliente(id_usuario)
---     ON DELETE CASCADE
---     ON UPDATE CASCADE
--- );
---
--- -- Tabela FotoProfissional
--- CREATE TABLE IF NOT EXISTS foto_profissional (
---     id INT AUTO_INCREMENT,
---     profissional_id INT NOT NULL,
---     foto BLOB NOT NULL,
---     CONSTRAINT pk_foto_profissional PRIMARY KEY (id),
---     CONSTRAINT fk_foto_profissional_profissional FOREIGN KEY (profissional_id) REFERENCES profissional(id_usuario)
---     ON DELETE CASCADE
---     ON UPDATE CASCADE
--- );
-
--- Tabela Login
-CREATE TABLE IF NOT EXISTS login (
-                                     id_usuario INT NOT NULL REFERENCES usuario(id),
+-- Tabela Usuario
+CREATE TABLE IF NOT EXISTS usuario (
+    id INT NOT NULL AUTO_INCREMENT,
     login VARCHAR(100) NOT NULL UNIQUE,
     senha VARCHAR(256) NOT NULL,
-    CONSTRAINT pk_login PRIMARY KEY(id_usuario)
-    );
+    CONSTRAINT pk_usuario PRIMARY KEY(id)
+);
+
+-- Tabela FotoCliente
+CREATE TABLE IF NOT EXISTS foto_cliente (
+    id INT AUTO_INCREMENT,
+    cliente_id INT NOT NULL,
+    foto BLOB NOT NULL,
+    CONSTRAINT pk_foto_cliente PRIMARY KEY (id),
+    CONSTRAINT fk_foto_cliente_cliente FOREIGN KEY (cliente_id) REFERENCES cliente(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+-- Tabela FotoProfissional
+CREATE TABLE IF NOT EXISTS foto_profissional (
+    id INT AUTO_INCREMENT,
+    profissional_id INT NOT NULL,
+    foto BLOB NOT NULL,
+    CONSTRAINT pk_foto_profissional PRIMARY KEY (id),
+    CONSTRAINT fk_foto_profissional_profissional FOREIGN KEY (profissional_id) REFERENCES profissional(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
 
--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,55 +1,38 @@
 -- Inserir dados na tabela Usuario
-INSERT INTO usuario (nome, email, senha, telefone, cep, numero_endereco, complemento_endereco, tipo)
+INSERT INTO cliente (nome, email, senha, telefone, cep, numero_endereco, complemento_endereco, cpf)
 VALUES
-    ('Ana Amalia', 'ana@mail.com', 'senha', '(11) 99999-0001', '88111-111', '001', 'Apto 101', 'cliente'),
-    ('Bia Bernardes', 'Bia@mail.com', 'senha', '(21) 98888-0002', '88222-222', '002', 'Casa', 'cliente'),
-    ('Dany Damaris', 'dany@mail.com', 'senha', '(31) 97777-0003', '88333-333', '003', 'Sala 3', 'profissional'),
-    ('Emy Esteves', 'emy@mail.com', 'senha', '(41) 96666-0004', '88333-444', '004', 'casa', 'profissional');
+    ('Ana Amalia', 'ana@mail.com', 'senha1111', '(11) 99999-0001', '88111-111', '001', 'Apto 101', '11111111111'),
+    ('Bia Bernardes', 'Bia@mail.com', 'senha2222', '(21) 98888-0002', '88222-222', '002', 'Casa', '22222222222');
 
--- Inserir dados na tabela Cliente
-INSERT INTO cliente (id_usuario, cpf, nascimento)
-VALUES
-    (1, '11111111111', '1955-01-12'),
-    (2, '22222222222', '2000-05-05');
 
 -- Inserir dados na tabela Profissional
-INSERT INTO profissional (id_usuario, especialidade, registro_profissional)
+INSERT INTO profissional (nome, email, senha, telefone, cep, numero_endereco, complemento_endereco, especialidade, registro_profissional)
 VALUES
-    (3, 'Massoterapia', 'REG33333'),
-    (4, 'Parteira', 'REG44444');
+    ('Dany Damaris', 'dany@mail.com', 'senha3333', '(31) 97777-0003', '88333-333', '003', 'Sala 3', 'Massoterapia', 'REG33333'),
+    ('Emy Esteves', 'emy@mail.com', 'senha4444', '(41) 96666-0004', '88333-444', '004', 'casa', 'Parteira', 'REG44444');;
 
 -- Inserir dados na tabela Servico
 INSERT INTO servico (nome, descricao, preco, duracao, situacao, profissional_id, data_disponivel)
 VALUES
-    ('Massagem Relaxante', 'Sessão de massagem para aliviar o estresse e tensões', 120.00, 60, 'ATIVO', 3, '2025-01-12'),
-    ('Massagem Terapêutica', 'Sessão terapêutica para tratar dores específicas', 150.00, 90, 'INATIVO', 3, '2024-01-12'),
-    ('Parto Humanizado', 'Acompanhamento do parto com respeito e cuidado', 2000.00, 180, 'ATIVO', 4, '2024-02-22'),
-    ('Acolhimento Primeiros Dias', 'Acompanhamento dos primeiros dias em sua jornada nesse maravilhoso universo da maternidade.', 300.00, 180, 'ATIVO', 4, '2024-02-22'),
-    ('Massagem Relaxante', 'Sessão de massagem para aliviar o estresse e tensões', 120.00, 60, 'ATIVO', 3,'2025-01-12');
+    ('Massagem Relaxante', 'Sessão de massagem para aliviar o estresse e tensões', 120.00, 60, 'ATIVO', 1, '2025-01-12'),
+    ('Massagem Terapêutica', 'Sessão terapêutica para tratar dores específicas', 150.00, 90, 'INATIVO', 1, '2024-01-12'),
+    ('Parto Humanizado', 'Acompanhamento do parto com respeito e cuidado', 2000.00, 180, 'ATIVO', 2, '2024-02-22'),
+    ('Massagem Relaxante', 'Sessão de massagem para aliviar o estresse e tensões', 120.00, 60, 'ATIVO', 1,'2025-01-12');
 
 -- Inserir dados na tabela Agendamento
-INSERT INTO agendamento (cliente_id, servico_id, data_hora, status)
+INSERT INTO agendamento (cliente_id, servico_id, data_hora)
 VALUES
-    (1, 1, '2024-01-12 10:00:00', 'CONFIRMADO'),
-    (2, 2, '2024-01-13 14:00:00', 'CONFIRMADO'),
-    (1, 3, '2024-01-14 09:00:00', 'CONFIRMADO'),
-    (1, 4, '2024-01-14 09:00:00', 'PENDENTE'),
-    (2, 1, '2024-01-15 11:00:00', 'CANCELADO');
+    (1, 1, '2024-01-12 10:00:00'),
+    (2, 2, '2024-01-13 14:00:00'),
+    (1, 3, '2024-01-14 09:00:00'),
+    (2, 1, '2024-01-15 11:00:00');
 
--- Inserir dados na tabela Avaliação
-INSERT INTO avaliacao (cliente_id, servico_id, data_hora, comentario, nota)
+INSERT INTO usuario (login, senha)
 VALUES
-    (1, 1, '2024-01-12 10:00:00', 'Ótimo serviço, muito profissional!', 5),
-    (2, 2, '2024-01-13 14:00:00', 'Bom atendimento, mas atrasou um pouco.', 4),
-    (1, 3, '2024-01-14 09:00:00','Atendimento excelente, recomendo!', 5 ),
-    (2, 1, '2024-01-15 11:00:00', 'Não gostei muito, esperava algo melhor.', 2);
-
-INSERT INTO login (id_usuario, login, senha)
-VALUES
-    (1,'ana@mail.com', 'senha1111'),
-    (2,'Bia@mail.com', 'senha2222'),
-    (3,'dany@mail.com', 'senha3333'),
-    (4,'emy@mail.com', 'senha4444');
+    ('ana@mail.com', 'senha1111'),
+    ('Bia@mail.com', 'senha2222'),
+    ('dany@mail.com', 'senha3333'),
+    ('emy@mail.com', 'senha4444');
 
 
 
